0215626cc78e56ace6204abfa23a2c713721c508
EricJeffrey <1719937412@qq.com>
2020-09-02 20:11:58 +0800

    make pull more robust

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@
 
 #include "pull.h"
 
+// todo command line sub command args
+// todo get image config
+
 int main(int argc, char const *argv[]) {
     try {
         Logger::init(std::cerr);
         loggerInstance()->setDebug(true);
-        pull("python", "alpine");
+        // pull("python", "latest");
+        pull("python:3.6.3-stretch");
     } catch (const std::exception &e) {
         std::cerr << "Pull failed with exception: " << e.what() << '\n';
     }
--- pull.h
+++ pull.h
@@ -6,80 +6,177 @@
 #include "httplib.h"
 #include "json.hpp"
 
+#include <openssl/sha.h>
 #include <sstream>
 #include <ostream>
+#include <iostream>
 #include <set>
 #include <map>
 #include <string>
 #include <cstdio>
+#include <vector>
 
-typedef const string &constrref;
+const string imageDirPath = "./build/images/";
+const string layerDirPath = "./build/layers/";
 
-const string registryAddr = "https://ejlzjv4p.mirror.aliyuncs.com";
+const string defaultRegAddr = "https://ejlzjv4p.mirror.aliyuncs.com";
+// const string defaultRegAddr = "https://docker.mirrors.ustc.edu.cn";
+const time_t readTimeoutInSec = 30;
 
-void checkAndThrow(const httplib::Result &resp, const string &msg) {
-    using std::runtime_error;
-    if (resp.error()) {
-        loggerInstance()->error({msg, to_string(resp.error())});
-        throw runtime_error("http request failed");
-    } else if (resp->status != 200) {
-        loggerInstance()->error({msg, to_string(resp->status)});
-        throw runtime_error("http resposne invalid");
+enum RegistryEndPoint { CHECK = 0, IMAGE_MANIFESTS, IMAGE_BLOBS };
+class RegistryHelper {
+public:
+    static string getPath(RegistryEndPoint endPoint, std::vector<string> args = {}) {
+        std::stringstream ss;
+        switch (endPoint) {
+            case CHECK:
+                ss << "/v2/";
+                break;
+            case IMAGE_MANIFESTS:
+                ss << "/v2/library/" << args[0] << "/manifests/" << args[1];
+                break;
+            case IMAGE_BLOBS:
+                ss << "/v2/library/" << args[0] << "/blobs/" << args[1];
+                break;
+            default:
+                break;
+        }
+        return ss.str();
     }
+};
+
+// string sha256_string(const char *str, size_t len) {
+//     char output_buffer[65];
+//     std::fill(output_buffer, output_buffer + 65, 0);
+//     unsigned char hash[SHA256_DIGEST_LENGTH];
+//     SHA256_CTX sha256;
+//     SHA256_Init(&sha256);
+//     SHA256_Update(&sha256, str, len);
+//     SHA256_Final(hash, &sha256);
+//     int i = 0;
+//     for (i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+//         sprintf(output_buffer + (i * 2), "%02x", hash[i]);
+//     }
+//     return string(output_buffer);
+// }
+
+int pull(const string &imgNameTag, const string &regAddr = defaultRegAddr);
+int pull(const string &imgName, const string &tag, const string &regAddr);
+
+int pull(const string &imgNameTag, const string &regAddr) {
+    string imgName, tag;
+    const size_t colonPos = imgNameTag.find(':');
+    if (colonPos != imgNameTag.size()) {
+        imgName = imgNameTag.substr(0, colonPos);
+        tag = imgNameTag.substr(colonPos + 1);
+    } else {
+        imgName = imgNameTag, tag = "latest";
+    }
+    return pull(imgName, tag, regAddr);
 }
 
-int pull(constrref imgName, constrref tag) {
-    httplib::Client client(registryAddr.c_str());
-    // check registry version
-    client.set_read_timeout(30);
-    auto resp = client.Get("/v2/");
-    checkAndThrow(resp, "Get /v2/ failed");
-    // get manifest
-    loggerInstance()->debug({"fetching image:" , imgName, ":", tag, "from", registryAddr});
-    httplib::Headers header;
-    header.insert(std::make_pair("Accept", "application/vnd.docker.distribution.manifest.v1+json"));
-    resp = client.Get(("/v2/library/" + imgName + "/manifests/" + tag).c_str());
-    checkAndThrow(resp, "GET /v2/.../manifests/... failed");
-    loggerInstance()->debug("manifest fetched, pulling layers");
-    // store manifest
-    nlohmann::json respJson = nlohmann::json::parse(resp->body);
-    std::set<string> blobSet;
-    for (auto &&blob : respJson["fsLayers"]) {
-        const string tmpBlobSum = blob["blobSum"].dump();
-        blobSet.insert(tmpBlobSum.substr(1, tmpBlobSum.size() - 2));
+// No throw
+int pull(const string &imgName, const string &tag, const string &regAddr) {
+    httplib::Client client(regAddr.c_str());
+    client.set_read_timeout(readTimeoutInSec);
+
+    loggerInstance()->info({"Connecting to registry:", regAddr});
+    // check connection
+    httplib::Result resp = client.Get(RegistryHelper::getPath(RegistryEndPoint::CHECK).c_str());
+    if (resp.error() || resp->status != 200) {
+        if (resp.error())
+            loggerInstance()->error({"Connect to registry:", regAddr, "failed"});
+        else
+            loggerInstance()->error(
+                {"Cannot fetch image from registry:", regAddr, "v2 not supported"});
+        return -1;
     }
 
+    loggerInstance()->info("Fetching image manifest");
+    // get manifest
+    httplib::Headers headers(
+        {std::make_pair("Accept", "application/vnd.docker.distribution.manifest.v2+json")});
+    resp = client.Get(
+        RegistryHelper::getPath(RegistryEndPoint::IMAGE_MANIFESTS, {imgName, tag}).c_str(),
+        headers);
+    if (resp.error() || resp->status != 200) {
+        if (resp.error())
+            loggerInstance()->error({"Fetch image manifest from registry:", regAddr, "failed"});
+        else
+            loggerInstance()->info({"Image:", imgName + ":" + tag, "not found"});
+        return -1;
+    }
+    // parse manifest
+    string manifest = resp->body;
+    string manifestSha256;
+    nlohmann::json manifestJson;
+    std::set<string> blobSumSet;
+    try {
+        // manifestSha256 = sha256_string(manifest.c_str(), manifest.size());
+        manifestSha256 = resp->headers.find("Docker-Content-Digest")->second.substr(7);
+        manifestJson = nlohmann::json::parse(manifest);
+        for (auto &&blob : manifestJson["layers"])
+            blobSumSet.insert(blob["digest"].get<string>());
+    } catch (const std::exception &e) {
+        loggerInstance()->error({"Parsing manifest failed:", e.what()});
+        return -1;
+    }
+
+    // fetch blobs
+    loggerInstance()->info("Fetching image layers");
+    client.set_follow_location(true);
     std::cerr << std::fixed << std::setprecision(2);
     try {
-        for (auto &&blobSum : blobSet) {
-            size_t totalSize = 0, curSize = 0;
-            const string blobAddr = "/v2/library/" + imgName + "/blobs/" + blobSum;
-            ofstream fileOStream("build/layers/" + blobSum.substr(7) + ".tar.gz", std::ios::out);
-            client.set_follow_location(true);
-            auto blobResp = client.Get(
-                (blobAddr).c_str(), httplib::Headers(),
-                [&](const httplib::Response &curResp) {
-                    if (curResp.status != 200 && curResp.status / 100 != 3) {
-                        loggerInstance()->error({"无效的http响应码: ", to_string(curResp.status)});
-                        throw std::runtime_error("http resposne status invalid");
+        for (auto &&blobSum : blobSumSet) {
+            size_t totalSize = 0, receivedSize = 0;
+            const string blobAddr =
+                RegistryHelper::getPath(RegistryEndPoint::IMAGE_BLOBS, {imgName, blobSum});
+            ofstream layerFStream(layerDirPath + blobSum.substr(7) + ".tar.gz", std::ios::out);
+            httplib::Result blobResp = client.Get(
+                blobAddr.c_str(), httplib::Headers(),
+                [&](const httplib::Response &tmpResp) {
+                    if (tmpResp.status != 200 && tmpResp.status / 100 != 3) {
+                        loggerInstance()->error(
+                            {"Fetch image layer failed, invalid response code"});
+                        return false;
                     }
-                    totalSize = std::stoi(curResp.headers.find("Content-Length")->second);
+                    totalSize = std::stoul(tmpResp.headers.find("Content-Length")->second);
                     return true;
                 },
                 [&](const char *data, size_t dataLength) {
-                    fileOStream.write(data, dataLength);
-                    curSize += dataLength;
+                    layerFStream.write(data, dataLength);
+                    receivedSize += dataLength;
                     std::cerr << "\r" << blobSum.substr(7, 16) << ": "
-                              << curSize * 1.0 / totalSize * 100 << "%";
+                              << receivedSize * 1.0 / totalSize * 100 << "%";
                     return true;
                 });
-            fileOStream.close();
+            layerFStream.flush();
+            layerFStream.close();
             std::cerr << endl;
         }
     } catch (const std::exception &e) {
-        loggerInstance()->error({"镜像获取失败", e.what()});
+        loggerInstance()->error({"Fetch image layer failed:", e.what()});
+        return -1;
     }
 
+    loggerInstance()->info("Storing image");
+    // store image
+    int ret = mkdir((imageDirPath + manifestSha256).c_str(), S_IRWXU | S_IRWXG | S_IROTH);
+    if (ret == -1) {
+        loggerInstance()->sysError(errno,
+                                   "Make image dir: " + imageDirPath + manifestSha256 + " failed");
+        return -1;
+    }
+    try {
+        const string formattedManifest = manifestJson.dump(4);
+        ofstream manifestOFStream(imageDirPath + manifestSha256 + "/manifest", std::ios::out);
+        manifestOFStream.write(formattedManifest.c_str(), formattedManifest.size());
+    } catch (const std::exception &e) {
+        loggerInstance()->error({"Write manifest failed:", e.what()});
+        return -1;
+    }
+    loggerInstance()->info(
+        {"Image", imgName + ":" + tag, "stored at:", imageDirPath + manifestSha256});
     return 0;
 }
 
